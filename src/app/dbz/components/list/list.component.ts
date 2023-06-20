9d2af7e4be7649c9c6fb41d746fac932ac8cfc40
Roberto Velazquez <roberto.velazquez@pricetravel.com>
2023-06-20 08:10:00 -0600

    primer comit

---
+++ src/app/dbz/components/list/list.component.ts
@@ -0,0 +1,23 @@
+import { Component, EventEmitter, Input, Output } from '@angular/core';
+import { Character } from '../../interfaces/character.interface';
+
+@Component({
+  selector: 'dbz-list',
+  templateUrl: './list.component.html',
+  styleUrls: ['./list.component.css']
+})
+export class ListComponent {
+  @Output()
+  public onDeleteCharacter: EventEmitter<string> = new EventEmitter()
+
+  @Input()
+  public characterList: Character[] = [{
+    name: 'Trunks',
+    power: 10
+  }];
+
+  emitDeleteCharacter(id?:string):void {
+    console.log(id)
+    this.onDeleteCharacter.emit(id);
+  }
+}
